db4dacf15e60d1230d72a579cbdd42f6163ad066
filipejessica64@gmail.com <filipejessica64@gmail.com>
2025-10-07 11:14:58 +0200

    Add project files.

---
+++ pokemonApp/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemonApp
+{
+    public class Pokemon
+    {
+		//Construtor
+		public Pokemon()
+		{ 
+			this.nome = "";
+			this.descricao = "";
+        }
+        //Propriedades(ee uma caracteristica do objeto)
+        private String  nome;
+
+		public String  Nome
+		{
+			get { return nome; }
+			set {
+				String texto = value.ToUpper();
+				nome = texto; 
+			}
+		}
+		private String descricao;
+
+		public String Descricao
+		{
+			get { return this.descricao; }
+			set { 
+				this.descricao = value.ToUpper();
+			}
+		}
+		//Metodo( ee uma acao que o objeto pode fazer)
+		public void ExibirDados()
+		{
+			Console.WriteLine("Nome do Pokemon: " + this.nome);
+			Console.WriteLine("Descricao do Pokemon: " + this.descricao);
+        }
+
+    }
+}
---
+++ pokemonApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon p = new Pokemon();
+            p.Nome = "Bulbasauro";
+            p.Descricao = "Bulbasauro ee um pokemon tipo planta";
+            p.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
